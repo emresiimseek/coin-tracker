bd05dd09d30b30032ea299e69919bf1b631331ce
Emre Simsek <emresimseka@gmail.com>
2023-03-25 14:07:21 +0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import BitcoinPrice from "./Paribu";
 
 function App() {
+  const [coins, setCoins] = useState<any[]>([]);
+  const [usdttry, setUsdttry] = useState<any>({});
+  const [price, setPrice] = useState(null);
+
+  useEffect(() => {
+    const socket2 = new WebSocket("wss://stream.binance.com/ws");
+    socket2.onopen = () => {
+      console.log("WebSocket connected 2");
+      // Tüm piyasa çiftlerinin ticker verilerini isteyin
+      const request2 = {
+        method: "SUBSCRIBE",
+        params: ["usdttry@ticker"],
+        id: 2,
+      };
+      socket2.send(JSON.stringify(request2));
+    };
+
+    socket2.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      setUsdttry(data);
+    };
+
+    const socket = new WebSocket("wss://fstream.binance.com/ws");
+
+    socket.onopen = () => {
+      console.log("WebSocket connected");
+      // Tüm piyasa çiftlerinin ticker verilerini isteyin
+      const request = {
+        method: "SUBSCRIBE",
+        params: ["!miniTicker@arr"],
+        id: 1,
+      };
+      socket.send(JSON.stringify(request));
+    };
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      setCoins(data);
+    };
+
+    return () => {
+      socket.close();
+      socket2.close();
+    };
+  }, []);
+  const columns = coins.length ? Object.keys(coins[0]) : [];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <BitcoinPrice />
+      {/* <div>{usdttry.c} </div> */}
+      {!!coins.length && (
+        <table border={1}>
+          <thead>
+            <tr>
+              {columns.map((col) => (
+                <th key={col}>{col}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((row, index) => (
+              <tr key={index}>
+                {columns.map((col) => (
+                  <td key={col}>
+                    {col === "c" ? row[col] * usdttry.c : row[col]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
