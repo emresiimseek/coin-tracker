c2ce3ef627e7a2e3b5cb76068a8f6e583d2ad89b
Emre Simsek <emresimseka@gmail.com>
2023-03-25 15:48:34 +0300

    Combine table and list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,88 @@
-import { useEffect, useState } from "react";
-import BitcoinPrice from "./Paribu";
+import { useEffect, useMemo, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [coins, setCoins] = useState<any[]>([]);
+  const [binanceCoins, setBinanceCoins] = useState<any[]>([]);
   const [usdttry, setUsdttry] = useState<any>({});
-  const [price, setPrice] = useState(null);
+  const [paribusCoins, setParibuCoins] = useState<any[]>([]);
+  const [combinedArray, setCombinedArray] = useState<any[]>([]);
+
+  function mergeArrays(arr1: any[], arr2: any[]) {
+    // Birinci dizideki her öğe için
+
+    if (!Array.isArray(arr1) || !Array.isArray(arr2)) return [];
+
+    const mergedArray = arr1.map((item1) => {
+      // İkinci dizideki öğeleri döngüye alarak eşleştir
+      const item2 = arr2.find((item) => {
+        const d1 = item.symbol.split("_")[0];
+
+        if (d1 === "USDT") return false;
+        return item1.s.includes(d1);
+      });
+      // Eğer öğeler eşleşirse
+      if (item2) {
+        // Yeni bir nesne oluşturarak birleştir
+
+        const data = {
+          symbolBinance: item1.s,
+          priceBinance: Number(item1.c) * Number(usdttry.c),
+          symbolParibu: item2.symbol,
+          priceParibu: item2.last,
+        };
+
+        if (Number.isNaN(data.priceBinance)) {
+          console.log(data);
+        }
+        return data;
+      }
+    });
+    // null olan tüm öğeleri filtrele
+    return mergedArray.filter((item) => !!item);
+  }
+
+  useMemo(() => {
+    if (!paribusCoins.length || !binanceCoins.length || !usdttry) return;
+
+    const list = mergeArrays(binanceCoins, paribusCoins);
+
+    const uniqueArr: any[] = [];
+
+    list.forEach((obj) => {
+      if (
+        !uniqueArr.some(
+          (uniqueObj) => uniqueObj.symbolBinance === obj?.symbolBinance
+        )
+      ) {
+        uniqueArr.push(obj);
+      }
+    });
+
+    setCombinedArray(uniqueArr);
+  }, [usdttry, paribusCoins, binanceCoins]);
 
   useEffect(() => {
+    const getPrice = async () => {
+      axios
+        .get("https://www.paribu.com/ticker")
+        .then((response) => {
+          const bitcoinPrice = response.data;
+          const dataArray = Object.entries(bitcoinPrice).map(
+            ([symbol, data1]) => ({
+              symbol,
+              ...(data1 as any),
+            })
+          );
+          setParibuCoins(dataArray);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    const interval = setInterval(() => {
+      getPrice();
+    }, 1000);
+
     const socket2 = new WebSocket("wss://stream.binance.com/ws");
     socket2.onopen = () => {
       console.log("WebSocket connected 2");
@@ -21,6 +97,8 @@ function App() {
 
     socket2.onmessage = (event) => {
       const data = JSON.parse(event.data);
+
+      if (!data) return;
 
       setUsdttry(data);
     };
@@ -40,43 +118,42 @@ function App() {
 
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data);
+      if (!data) return;
 
-      setCoins(data);
+      setBinanceCoins(data);
     };
-
     return () => {
       socket.close();
       socket2.close();
+      clearInterval(interval);
     };
   }, []);
-  const columns = coins.length ? Object.keys(coins[0]) : [];
+  const columns = binanceCoins.length ? Object.keys(binanceCoins[0]) : [];
 
   return (
     <div style={{ display: "flex" }}>
-      <BitcoinPrice />
-      {/* <div>{usdttry.c} </div> */}
-      {!!coins.length && (
-        <table border={1}>
-          <thead>
-            <tr>
-              {columns.map((col) => (
-                <th key={col}>{col}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {coins.map((row, index) => (
-              <tr key={index}>
-                {columns.map((col) => (
-                  <td key={col}>
-                    {col === "c" ? row[col] * usdttry.c : row[col]}
-                  </td>
-                ))}
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Symbol Binance</th>
+            <th>Price Binance</th>
+            <th>Symbol Paribu</th>
+            <th>Price Paribu</th>
+          </tr>
+        </thead>
+        <tbody>
+          {combinedArray.map(
+            ({ symbolBinance, priceBinance, symbolParibu, priceParibu }) => (
+              <tr key={priceBinance + priceParibu}>
+                <td>{symbolBinance}</td>
+                <td>{priceBinance}</td>
+                <td>{symbolParibu}</td>
+                <td>{priceParibu}</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            )
+          )}
+        </tbody>
+      </table>{" "}
     </div>
   );
 }
